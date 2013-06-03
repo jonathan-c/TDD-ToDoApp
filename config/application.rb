f2aea694746e3b0b2f89706bce4dd8bdd6ba5bfa
Jonathan Castillo <joncastillo123@gmail.com>
2013-06-03 18:26:07 -0400

    Users can sign in with email address

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,13 @@ end
 
 module Todos
   class Application < Rails::Application
+    # No specs during generations
+    # config.generators do |g|
+    #   g.view_specs false
+    #   g.helper_specs false
+    # end
+    # 
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Todos::Application.routes.draw do
   root to: 'high_voltage/pages#show', id: 'homepage'
+  
+  resource :session, only: [:create]
 
 end
